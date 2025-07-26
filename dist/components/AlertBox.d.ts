4b7dabd54185f008f39dc39f3d9c335d29d1177d
manishkhanchandani <naveenkhanchandani@gmail.com>
2025-07-25 21:05:46 -0700

    more components

--- dist/components/AlertBox.d.ts
+++
@@ -1,6 +0,0 @@
-declare const AlertBox: ({ message, title, severity, }: {
-    message?: string;
-    title: string;
-    severity?: "error" | "info" | "success" | "warning";
-}) => import("react/jsx-runtime").JSX.Element;
-export default AlertBox;
